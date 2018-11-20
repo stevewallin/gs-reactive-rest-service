f188fcef78dd32ff872fa32c82fa82107bf5218e
Maarten Smeets <wisp1980@gmail.com>
2018-11-20 08:18:49 +0000

    Adding OpenJDK and extra logging

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,9 +2,17 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+  		return registry -> registry.config().commonTags("application", "MYAPP");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
